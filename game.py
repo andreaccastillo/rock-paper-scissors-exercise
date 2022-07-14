79173df3f220b224a77856777ab7c8d218d4ac94
Mona Lisa <acr515@stern.nyu.edu>
2022-07-14 17:52:09 -0400

    Determine winner

--- game.py
+++ game.py
@@ -25,6 +25,22 @@ computer_choice = random.choice(valid_options)
 print(f"computer chose:", computer_choice)
 
 #DETERMINING THE WINNER
-
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user_choice == "paper":
+    if computer == "rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
 
 #DISPLAYING RESULTS
