8be72aa31ae3972fc76d3d921dea56717b47c6b2
Mona Lisa <acr515@stern.nyu.edu>
2022-07-14 17:36:30 -0400

    computer choice

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 # this is the "game.py" file...
 
+import random
 from xml.sax.xmlreader import InputSource
-
 
 print("Rock, Paper, Scissors, Shoot!")
 
@@ -14,13 +14,15 @@ user_choice = input( "Please make a selection ( 'rock', 'paper', 'scissors'):")
 # You chose rock
 print (f"You chose: '{user_choice}' ")
 
-
-
 #VALIDATE USER INPUTS
 
 
 #COMPUTER CHOICE
 
+#import random
+valid_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(valid_options)
+print(f"computer chose:", computer_choice)
 
 #DETERMINING THE WINNER
 
