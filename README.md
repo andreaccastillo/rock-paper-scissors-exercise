34e770ca0bbac52df73cacfa2816d48ea4a191c2
Mona Lisa <acr515@stern.nyu.edu>
2022-07-14 17:01:29 -0400

    User choice

--- README.md
+++ README.md
@@ -6,11 +6,14 @@ Here is a shell/terminal command
 ```sh
 python game.py
 ```
+print("WELCOME TO THE ROCK PAPER SCISSORS GAME!")
+
 
 
 
 #USER INPUTS
 
+user_choice = input("Please make selection")
 
 
 #VALIDATE USER INPUTS
--- game.py
+++ game.py
@@ -1,3 +1,28 @@
 # this is the "game.py" file...
 
+from xml.sax.xmlreader import InputSource
+
+
 print("Rock, Paper, Scissors, Shoot!")
+
+
+
+#USER INPUTS
+
+user_choice = input( "Please make a selection ( 'rock', 'paper', 'scissors'):")
+
+# You chose rock
+print (f"You chose: '{user_choice}' ")
+
+
+
+#VALIDATE USER INPUTS
+
+
+#COMPUTER CHOICE
+
+
+#DETERMINING THE WINNER
+
+
+#DISPLAYING RESULTS
